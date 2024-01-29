2295b99e5efda92438a16db5184954a45fa1e501
sawyer bristol <sawyerbristol@gmail.com>
2024-01-28 22:03:10 -0700

    pre-commit

---
+++ src/auto_correct.c
@@ -0,0 +1,48 @@
+#include <string.h>
+
+typedef char word[10];
+
+// this is for english letters
+// should be varible for config later
+#define N 26 
+
+typedef struct TrieNode TrieNode;
+
+struct TrieNode {
+    // The Trie Node Structure
+    // Each node has N children, starting from the root
+    // and a flag to check if it's a leaf node
+    char data; // Storing for printing purposes only
+    TrieNode* children[N];
+    int is_leaf;
+};
+
+TrieNode* make_trienode(char data) {
+    // Allocate memory for a TrieNode
+    TrieNode* node = (TrieNode*) calloc (1, sizeof(TrieNode));
+    for (int i=0; i<N; i++)
+        node->children[i] = NULL;
+    node->is_leaf = 0;
+    node->data = data;
+    return node;
+}
+
+void free_trienode(TrieNode* node) {
+    // Free the trienode sequence
+    for(int i=0; i<N; i++) {
+        if (node->children[i] != NULL) {
+            free_trienode(node->children[i]);
+        }
+        else {
+            continue;
+        }
+    }
+    free(node);
+}
+
+struct correction_buffer {
+  word last_word;
+  TrieNode nodes;
+};
+
+
