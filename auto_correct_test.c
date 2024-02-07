3578b911744431de689ef99efaf3e196250f59d3
sawyer bristol <sawyerbristol@gmail.com>
2024-02-06 21:04:45 -0700

    remove building functions for trie

--- auto_correct_test.c
+++ auto_correct_test.c
@@ -14,102 +14,15 @@
 #include <stdio.h>
 #include <string.h>
 
-#define DICT_NAME "words.txt"
-#define DICT_SIZE ((int)get_dict_size())
+#include "trie_dict.h"
 
-int get_dict_size() {
-    FILE *fp;
-    int count = 0;
-    char c;
-    fp = fopen(DICT_NAME, "r");
-    while ((c = fgetc(fp)) != EOF) {
-        if (c == '\n') {
-            ++count;
-        }
-    }
-    fclose(fp);
-    return count;
-}
+#define N 26 // 26 lowercase english letters
 
-#define WORD_LIST ((char **)get_dict())
-
-char **get_dict() {
-    const int max_string_len = 35; // WARNING: 35 is the max length string
-    FILE *fp = fopen(DICT_NAME, "r");
-
-    // get the file size to alloc
-    fseek(fp, 0, SEEK_END);
-    int size = ftell(fp);
-    fseek(fp, 0, SEEK_SET);
-
-    char **dict = malloc(sizeof(int *) * DICT_SIZE);
-    int loop_counter = 0;
-    char currentline[max_string_len];
-    while (fgets(currentline, sizeof(currentline), fp) != NULL) {
-        currentline[strlen(currentline) - 1] = '\0';
-        char *string = malloc(sizeof(currentline));
-        strcpy(string, currentline);
-        dict[loop_counter] = string;
-        ++loop_counter;
-    }
-    fclose(fp);
-
-    return dict;
-}
-
-#define N 26 // 26 letters - english
-
-struct TrieNode {
-    char data; // Storing for printing purposes only
-    struct TrieNode *children[N];
-    int is_leaf;
-};
-
-struct TrieNode *make_trienode(char data) {
-    // Allocate memory for a TrieNode
-    struct TrieNode *node = (struct TrieNode *)calloc(1, sizeof(struct TrieNode));
-    for (int i = 0; i < N; i++)
-        node->children[i] = NULL;
-    node->is_leaf = 0;
-    node->data = data;
-    return node;
-}
-
-void free_trienode(struct TrieNode *node) {
-    // Free the trienode sequence
-    for (int i = 0; i < N; i++) {
-        if (node->children[i] != NULL) {
-            free_trienode(node->children[i]);
-        } else {
-            continue;
-        }
-    }
-    free(node);
-}
-
-struct TrieNode *insert_trie(struct TrieNode *root, char *word) {
-    // Inserts the word onto the Trie
-    // ASSUMPTION: The word only has lower case characters
-    struct TrieNode *temp = root;
-
-    for (int i = 0; word[i] != '\0'; i++) {
-        // Get the relative position in the alphabet list
-        int idx = (int)word[i] - 'a';
-        if (temp->children[idx] == NULL) {
-            // If the corresponding child doesn't exist,
-            // simply create that child!
-            temp->children[idx] = make_trienode(word[i]);
-        } else {
-            // Do nothing. The node already exists
-        }
-        // Go down a level, to the child referenced by idx
-        // since we have a prefix match
-        temp = temp->children[idx];
-    }
-    // At the end of the word, mark this node as the leaf node
-    temp->is_leaf = 1;
-    return root;
-}
+// struct TrieNode {
+//     char data;
+//     struct TrieNode *children[N];
+//     int is_leaf;
+// };
 
 int search_trie(struct TrieNode *root, char *word) {
     // Searches for word in the Trie
@@ -134,16 +47,6 @@ void print_search(struct TrieNode *root, char *word) {
         printf("Found!\n");
 }
 
-#define DICT_TIRE (build_trie())
-
-struct TrieNode *build_trie() {
-    struct TrieNode *root = make_trienode('\0');
-    for (int i = 0; i < DICT_SIZE; ++i) {
-        root = insert_trie(root, WORD_LIST[i]);
-    }
-    return root;
-};
-
 typedef char word[15];
 
 struct LetterTable {
@@ -167,12 +70,7 @@ struct CorrectionBuffer buffer = {
 };
 
 int main() {
-    for (int i = 0; i < DICT_SIZE; ++i) {
-        printf("%s", WORD_LIST[i]);
-    }
-
-    struct TrieNode *root = DICT_TIRE;
-    print_search(root, "peer");
+    // print_search(root, "peer");
 
     return 0;
 }
